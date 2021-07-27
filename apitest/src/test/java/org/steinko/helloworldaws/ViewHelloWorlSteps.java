361c988d7a93dfaf59b4ed05e012318ceb0e09b3
steinko <steinkor@gmail.com>
2021-07-27 19:16:25 +0200

    implement then

--- apitest/src/test/java/org/steinko/helloworldaws/ViewHelloWorlSteps.java
+++ apitest/src/test/java/org/steinko/helloworldaws/ViewHelloWorlSteps.java
@@ -6,12 +6,15 @@ import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 
 import static io.restassured.RestAssured.given;
+import io.restassured.response.Response;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 
 public class ViewHelloWorlSteps {
+	
+	Response response;
 	
 	@Given("the HelloWorlsAWS application is runing")
 	public void the_hello_worls_aws_application_is_runing() {
@@ -21,17 +24,20 @@ public class ViewHelloWorlSteps {
 	@When("acivating URL {string}")
 	public void acivating_url(String url) {
 	
-	   	   
+		 response =   	   
 	   	   given()
 	       .when()
 	          .get(url)
 	       .then()
-	         .statusCode(200); 
+	         .statusCode(200)
+	         .contentType("text/plain")
+	       .extract()
+		       .response(); 
 	}
 
 	@Then("the HelloWorlsAWS application displays {string}")
 	public void the_hello_worls_aws_application_displays(String message) {
-	    assertEquals(message, "Hello AWS Elastic Beanstalk! From host: ip-172-31-6-15.eu-north-1.compute.internal/172.31.6.15" );
+	    assertEquals(message, response.getBody().asString());
 	}
 
 }
