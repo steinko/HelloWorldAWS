edf0771417d1a8cd570d6131f4616273ec4f2e2f
steinko <steinkor@gmail.com>
2021-07-27 08:25:16 +0200

    first commit

---
+++ src/main/java/org/steinko/HelloWorldAWS/HelloWorldAwsApplication.java
@@ -0,0 +1,13 @@
+package org.steinko.HelloWorldAWS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/steinko/HelloWorldAWS/HelloWorldAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.steinko.HelloWorldAWS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
