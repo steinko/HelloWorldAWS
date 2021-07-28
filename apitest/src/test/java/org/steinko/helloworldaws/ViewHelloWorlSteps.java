a3b4f0c00461b727ed61938d39a4f0df80e6b306
steinko <steinkor@gmail.com>
2021-07-28 06:56:22 +0200

    fix respons

--- apitest/src/test/java/org/steinko/helloworldaws/ViewHelloWorlSteps.java
+++ apitest/src/test/java/org/steinko/helloworldaws/ViewHelloWorlSteps.java
@@ -13,6 +13,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ViewHelloWorlSteps {
 	
+Response response;
+	
 	@Given("the HelloWorlsAWS application is runing")
 	public void the_hello_worls_aws_application_is_runing() {
 	   
@@ -21,17 +23,21 @@ public class ViewHelloWorlSteps {
 	@When("acivating URL {string}")
 	public void acivating_url(String url) {
 	
-	   	   
+		 response =   	   
 	   	   given()
 	       .when()
 	          .get(url)
 	       .then()
-	         .statusCode(200); 
+	         .statusCode(200)
+	         .contentType("text/plain")
+	       .extract()
+		       .response(); 
 	}
 
 	@Then("the HelloWorlsAWS application displays {string}")
 	public void the_hello_worls_aws_application_displays(String message) {
-	    assertEquals(message, "Hello AWS Elastic Beanstalk! From host: ip-172-31-6-15.eu-north-1.compute.internal/172.31.6.15" );
+	    assertEquals(message, response.getBody().asString());
 	}
+
 
 }
